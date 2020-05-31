2f3b22f54122a7810d86de73a8c97f3def9d8571
Mohsen Yousefian <me@manzik.com>
2020-05-31 08:17:39 -0500

    Added benchmark time series data averaging feature

--- cmdbench/utils.py
+++ cmdbench/utils.py
@@ -62,27 +62,104 @@ class BenchmarkResults():
     def get_averages(self):
 
         time_series_dict_key = "time_series"
+
         
-        time_series_x = deque([]) # A list of list of x values (in this case, milliseconds) to calculate y averages against
-        time_series_y_values = {} # Dict of list of possible y lists for each kind of resource
+        
         def avg_replace_func(list_of_objects, key_path):
             sample_data = list_of_objects[0]
             if(isinstance(sample_data, str)):
                 return None
             elif(key_path[0] == time_series_dict_key):
-                if(["sample_milliseconds", "exit_code"].index(key_path[1]) > -1):
-                    time_series_x.append(list_of_objects)
-                else:
-                    time_series_y_values[key_path[1]] = list_of_objects
+                return list_of_objects
             else:
                 return np.hstack(np.array(list_of_objects)).mean()
-
-        averaged_time_series = {}
-
-        # TODO: Implement average of the time series algorithm
         
         value_per_attribute_avgs_dict = self._get_values_per_attribute(self._iterations, avg_replace_func)
         
+        # Break down time series data to time_series_x_values and time_series_y_values
+        averaged_time_series = {}
+        # A list of list of x values (in this case, milliseconds) to calculate y averages against
+        time_series_x_values = value_per_attribute_avgs_dict["time_series"]["sample_milliseconds"]
+        
+        # Dict of list of possible y lists for each kind of resource (cpu percentages, memory values)
+        time_series_y_values = {}
+        
+        # Output (updated 1D average) values
+        time_series_x_values_out = []
+        time_series_y_values_out = {}
+        for key, value in  value_per_attribute_avgs_dict["time_series"].items():
+            if(key != "sample_milliseconds"):
+                time_series_y_values[key] = value
+                time_series_y_values_out[key] = []
+
+
+        # START: Average of the time series algorithm
+
+        time_series_np = np.hstack(np.array(time_series_x_values))
+        sample_min_ms = time_series_np.min()
+        sample_max_ms = time_series_np.max()
+        
+        samples_time_avg_max = np.array([np.array(time_series_x).max() for time_series_x in time_series_x_values]).mean()
+        samples_time_avg_min = np.array([np.array(time_series_x).min() for time_series_x in time_series_x_values]).mean()
+        samples_time_avg = samples_time_avg_max - samples_time_avg_min
+        samples_len_avg = np.array([len(time_series_x) for time_series_x in time_series_x_values]).mean()
+
+        # Average milliseconds per sample
+        avg_ms_per_sample = samples_time_avg / samples_len_avg
+
+        # Average y values based on each range for x values
+        # ranges' length are average milliseconds among x values we have
+        time_series_data_count = len(time_series_x_values)
+        scanning_time_indexes = [0] * time_series_data_count
+        for from_ms in np.arange(sample_min_ms, sample_max_ms, avg_ms_per_sample):
+            to_ms = from_ms + avg_ms_per_sample
+            
+            avging_indexes_list = [[] for _ in range(time_series_data_count)]
+
+            # Find list of indexes for each iteration of benchmarking that match the current
+            # range of milliseconds trying to calculate the average for
+            for avging_index_ind in range(time_series_data_count):
+                target_time_series_x = time_series_x_values[avging_index_ind]
+                while(scanning_time_indexes[avging_index_ind] < len(target_time_series_x) and target_time_series_x[scanning_time_indexes[avging_index_ind]] <= to_ms):
+                    avging_indexes_list[avging_index_ind].append(scanning_time_indexes[avging_index_ind])
+                    scanning_time_indexes[avging_index_ind] += 1
+
+            flattened_indexes = np.hstack(np.array(avging_indexes_list))
+            matching_range_indexes_count = len(flattened_indexes)
+            no_indexes_for_time_range = matching_range_indexes_count == 0
+            if(no_indexes_for_time_range):
+                continue
+            
+            # Calculate average of x values for matching indexes in the range
+            avg_x = 0 # i.e. average milliseconds
+            for avging_index_ind in range(time_series_data_count):
+                avging_indexes = avging_indexes_list[avging_index_ind]
+                for avging_index in avging_indexes:
+                    avg_x += time_series_x_values[avging_index_ind][avging_index]
+            avg_x /= matching_range_indexes_count
+            time_series_x_values_out.append(avg_x)
+
+            # Calculate average of y values of each key for matching indexes in the range
+            for key_y in time_series_y_values.keys():
+                # Y value for this key. Iterates through all keys and changes for each iteration
+                avg_key_y = 0
+                for avging_index_ind in range(time_series_data_count):
+                    avging_indexes = avging_indexes_list[avging_index_ind]
+                    for avging_index in avging_indexes:
+                        avg_key_y += time_series_y_values[key_y][avging_index_ind][avging_index]
+                avg_key_y /= matching_range_indexes_count
+
+                time_series_y_values_out[key_y].append(avg_key_y)
+
+        
+
+        # END: Average of the time series algorithm
+
+        # Pack data from time_series_x_values and time_series_y_values to averaged_time_series
+        # and finally time_series data
+        averaged_time_series["sample_milliseconds"] = time_series_x_values_out
+        for key, value in  time_series_y_values_out.items():
+            averaged_time_series[key] = value
         value_per_attribute_avgs_dict["time_series"] = averaged_time_series
 
         return BenchmarkDict.from_dict(value_per_attribute_avgs_dict)
